829fe9f04bc01976eada02b6a7d62c450483a394
AIMEE-421\aimee <eg25304016@ga.ttc.ac.jp>
2025-11-28 14:22:30 +0900

    GitInputSerclet.javaファイルを作りました

---
+++ WEB-INF/src/GitInputServlet.java
@@ -0,0 +1,37 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class RequestRecv extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        request.setCharacterEncoding("UTF-8");
+
+        
+        String lastName = request.getParameter("myoji");
+        String firstName = request.getParameter("namae");
+
+        
+        response.setContentType("text/html");
+        response.setCharacterEncoding("UTF-8");
+
+        
+        PrintWriter out = response.getWriter();
+        out.println("<!DOCTYPE html>");
+        out.println("<html>");
+        out.println("<head>");
+        out.println("<title>あなたの回答</title>");
+        out.println("</head>");
+        out.println("<body>");
+        out.println("<h1>あなたの回答</h1>");
+        out.println("姓は" + lastName + "ですね。");
+        out.println("名は" + firstName + "ですね。");
+        out.println("</body>");
+        out.println("</html>");
+    }
+}
